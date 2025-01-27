1ba180b620f74586bddfd1c8c456f4c8e76631f1
НовыйИмя <Email>
2025-01-27 19:40:49 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { createRoot } from 'react-dom/client'; // React 18+
+import App from './App'; // Подключаем главный компонент
+import './css/profile.css'; // Подключаем стили
+
+const container = document.getElementById('root');
+const root = createRoot(container);
+
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
