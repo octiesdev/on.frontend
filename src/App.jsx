d54dfabbb47d1ff805e30bdff730d1808f2309b2
НовыйИмя <Email>
2025-01-27 19:08:24 +0100

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './css/profile.css'; // Подключаем ваш CSS
+
+const App = () => {
+  return (
+    <div>
+      {/* Пока ничего внутри — только фон */}
+    </div>
+  );
+};
+
+export default App;
