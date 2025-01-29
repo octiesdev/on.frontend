acc89340d1d97e00064279d7342f2af5bbe334de
НовыйИмя <Email>
2025-01-29 11:42:01 +0100

    Fix BottomMenu import issue

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Profile from "./Profile";
 import Onexs from "./Onexs";
 import Tasks from "./Tasks";
-import BottomMenu from "../components/BottomMenu";
+import BottomMenu from "@components/BottomMenu";
 import "./App.module.css";
 
 function App() {
