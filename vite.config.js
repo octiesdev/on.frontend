603bd8070824b363fefb5ecb90f74c59d5a6abd6
НовыйИмя <Email>
2025-01-29 14:47:53 +0100

    fix

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,10 @@ export default defineConfig({
   plugins: [react()],
   build: {
     outDir: 'dist', 
-  }
+  },
+  "scripts": {
+  "dev": "vite", // Для запуска сервера разработки
+  "build": "vite build", // Для сборки проекта
+  "preview": "vite preview" // Для предварительного просмотра собранного проекта
+}
 });
