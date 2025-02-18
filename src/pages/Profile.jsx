712253feca1ec9b19874b4750c12f0b8c02e5bb1
НовыйИмя <Email>
2025-02-18 17:02:31 +0100

    Initial commit

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -87,7 +87,6 @@ const Profile = () => {
       <div className="rectangleAvaliableDrop size-medium pos-top-rectangleAvaliableDrop">
       <img src={onex50opacityImg} class="onex50opacity-icon" alt=""/>
       <div className="left-side">
-          <img src={onex50opacityImg} class="onex50opacity-icon" alt=""/>
           <p class="text01">03</p>
         </div>
       <div className="right-side">
