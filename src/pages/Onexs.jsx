95dc56b408ff91a1edb299ab9f39306bfa97008c
НовыйИмя <Email>
2025-02-19 20:47:16 +0100

    Initial commit

--- src/pages/Onexs.jsx
+++ src/pages/Onexs.jsx
@@ -8,30 +8,38 @@ const Onexs = () => {
     <div className="test-container">
       {/* Новый блок */}
       <div className="rectangleAvaliableDrop2">
-        <div className="header">
-          <img src={tonIMG} alt="TON Icon" className="ton-icon" />
-          <p className="ton-title">TON</p>
-        </div>
-
-        <div className="info-container">
-          <div className="farming-period">
-            <p>Период фарминга</p>
-            <p><strong>1 день</strong></p>
-          </div>
-          
-          <div className="available-onex">
-            <p>Доступно ONEX’s</p>
-            <p><span className="available">99</span> / 100</p>
-          </div>
-        </div>
-
-        <div className="reward-container">
-          <p>Награда в TON</p>
-          <p><strong>+1 TON</strong> <img src={tonIMG} alt="TON Icon" className="reward-icon" /></p>
-        </div>
-
-        <button className="start-farming">ЗАПУСТИТЬ БЕСПЛАТНО</button>
+      {/* Заголовок */}
+      <div className="header">
+        <img src={tonIcon} alt="TON" className="header-icon" />
+        <p className="header-text">TON</p>
       </div>
+
+      {/* Блок 1 - Период фарминга */}
+      <div className="info-block info-block-1">
+        <p className="info-text">Период фарминга</p>
+        <p className="bold-text">1 день</p>
+      </div>
+
+      {/* Блок 2 - Доступно ONEX's */}
+      <div className="info-block info-block-2">
+        <p className="info-text">Доступно ONEX’s</p>
+        <p className="bold-text">
+          <span className="green-text">99</span> / 100
+        </p>
+      </div>
+
+      {/* Блок 3 - Награда в TON */}
+      <div className="info-block info-block-3">
+        <p className="info-text">Награда в TON</p>
+        <div className="reward">
+          <p className="bold-text">+1 TON</p>
+          <img src={rewardIcon} alt="Reward Icon" className="reward-icon" />
+        </div>
+      </div>
+
+      {/* Кнопка */}
+      <div className="button">ЗАПУСТИТЬ БЕСПЛАТНО</div>
+    </div>
 
         <Footer />
     </div>
