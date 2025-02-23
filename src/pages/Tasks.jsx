8f265d2eee5fa672079ff6ca8a8ff86b5be64a0a
НовыйИмя <Email>
2025-02-23 20:34:46 +0100

    add tasks

--- src/pages/Tasks.jsx
+++ src/pages/Tasks.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import "../styles/Tasks.css";
 import logo from "../assets/logo.png";
+import buttonPartners from "../assets/buttonPartners.png";
+import buttonConnectWallet from "../assets/buttonConnectWallet.png";
 import Footer from "../Footer"; // Подключаем футер
 
 const Tasks = () => {
@@ -10,7 +12,8 @@ const Tasks = () => {
       <div className="ONEXs_Window">   
         <div className="Header">  
           <img src={logo} alt="Logo" className="headerlogo"/>
-
+          <img src={buttonPartners} alt="" className="headerButtonPartners"/>
+          <img src={buttonConnectWallet} alt="" className="headerConnectWalletConnected"/>
         </div>
       </div>
 
