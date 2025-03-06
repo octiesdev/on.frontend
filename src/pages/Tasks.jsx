2bbe03909323237b9187a3b4e649c86402058329
НовыйИмя <Email>
2025-03-06 17:17:40 +0100

    add tasks

--- src/pages/Tasks.jsx
+++ src/pages/Tasks.jsx
@@ -39,7 +39,7 @@ const Tasks = () => {
                   <img src={onexIMG}/>
                   <h2>ПРИСОЕДИНЯЙСЯ<br/>К ONEX!</h2>
                 </div>
-                <div className="number-OnexNode"> 
+                <div className="infoSection-all-text"> 
                   <h2>+10<br/>ONEX</h2>
                 </div>
               
