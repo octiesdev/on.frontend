e39b800f7c7dc668c01673fea73a81d51483e1be
НовыйИмя <Email>
2025-03-06 19:16:32 +0100

    add tasks

--- src/pages/Tasks.jsx
+++ src/pages/Tasks.jsx
@@ -39,7 +39,11 @@ const Tasks = () => {
                 <div className="infoSection-all-text"> 
                   <h2>+10<br/>ONEX</h2>
                 </div>
-              
+                <div className="task-to-be-complete-button"> 
+                  <div className="to-be-complete-button"> 
+                    ВЫПОЛНИТЬ
+                  </div>
+                </div>
               </div>
             </div>
           </div>
