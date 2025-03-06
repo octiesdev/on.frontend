b3c2cea7b3b7c66686ca46db3f34128f1776e27d
НовыйИмя <Email>
2025-03-06 19:36:16 +0100

    add tasks

--- src/pages/Tasks.jsx
+++ src/pages/Tasks.jsx
@@ -47,6 +47,24 @@ const Tasks = () => {
               </div>
             </div>
           </div>
+          <div className="onex-task2"> 
+            <div className="info-tasks-nameText">
+              <div className="info-section">
+                <div className="infoSection-all-logo"> 
+                  <img src={onexIMG}/>
+                  <h2>ПРИСОЕДИНЯЙСЯ<br/>К ONEX!</h2>
+                </div>
+                <div className="infoSection-all-text"> 
+                  <h2>+10<br/>ONEX</h2>
+                </div>
+              </div>
+              <div className="task-to-be-complete-button"> 
+                  <div className="to-be-complete-button"> 
+                    ВЫПОЛНИТЬ
+                  </div>
+              </div>
+            </div>
+          </div>
       </div>
     </div>
 
