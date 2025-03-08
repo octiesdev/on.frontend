c05ae0140565ecdbbf618d528867c41e6edec05d
НовыйИмя <Email>
2025-03-08 15:55:57 +0100

    add tasks

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,11 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  server: {
+    headers: {
+      "Access-Control-Allow-Origin": "*", // Разрешает доступ к ресурсам с любого домена
+      "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS", // Разрешённые HTTP-методы
+      "Access-Control-Allow-Headers": "Content-Type, Authorization" // Разрешённые заголовки
+    }
+  }
 });
