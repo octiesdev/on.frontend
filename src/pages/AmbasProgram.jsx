7c9946cb5ff6d9bb2c239738940b75309920348a
НовыйИмя <Email>
2025-03-12 13:29:35 +0100

    add tasks

--- src/pages/AmbasProgram.jsx
+++ src/pages/AmbasProgram.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { useNavigate } from "react-router-dom"; 
 import "../styles/AmbasProgram.css";
 import logo from "../assets/logo.png";
 import buttonPartners from "../assets/buttonPartners.png";
