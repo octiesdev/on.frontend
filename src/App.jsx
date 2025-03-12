44f8be0ff3ef903eca3cceff4dc558a7f8567e9b
НовыйИмя <Email>
2025-03-12 20:56:07 +0100

    connect wallet

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
+import { TonConnectUIProvider } from "@tonconnect/ui-react";
 import Profile from "./pages/Profile";
 import Tasks from "./pages/Tasks";
 import Onexs from "./pages/Onexs";
