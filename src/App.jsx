23ea90f30d909b2dbbbef6074099c19c9c4635f3
НовыйИмя <Email>
2025-03-12 23:43:17 +0100

    connect wallet

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect } from "react";
 import { Routes, Route } from "react-router-dom";
 import { TonConnectUIProvider } from "@tonconnect/ui-react";
+import { useTonConnectUI } from "@tonconnect/ui-react";
 import Profile from "./pages/Profile";
 import Tasks from "./pages/Tasks";
 import Onexs from "./pages/Onexs";
@@ -8,6 +9,16 @@ import AmbasProgram from "./pages/AmbasProgram";
 import OnAmbasProgram from "./pages/OnAmbasProgram";
 
 const App = () => {
+
+  const [tonConnectUI] = useTonConnectUI();
+
+  useEffect(() => {
+    tonConnectUI.getWallets()
+      .then(wallets => {
+        console.log("🔄 Доступные кошельки:", wallets);
+      })
+      .catch(err => console.error("Ошибка загрузки кошельков:", err));
+  }, []);
 
   return (
     <TonConnectUIProvider 
