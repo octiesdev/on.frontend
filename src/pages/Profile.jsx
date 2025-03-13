b05f46593e590ba19996dbf7822345940bef9598
НовыйИмя <Email>
2025-03-13 13:22:16 +0100

    connect wallet

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -50,7 +50,7 @@ const Profile = () => {
           </div>
             <div className="HeaderButtonsContainer">  
               <img src={buttonPartners} alt="" className="headerButtonPartners" onClick={() => navigate("/ambasProgram")}></img>
-              <TonConnectButton className="headerButtonPartners"/>
+              <TonConnectButton/>
             </div>
         </div>
         <div className="mainProfilePageContainer"> 
