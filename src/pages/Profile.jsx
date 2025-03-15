040c86fe6c5ed3f3dfdd4560e9880d3e441cd3a1
НовыйИмя <Email>
2025-03-15 13:54:44 +0100

    sendTransaction with payload

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { TonConnectButton, useTonAddress, useTonConnectUI } from "@tonconnect/ui-react";
+import { beginCell } from "@ton/core"; 
 import "../styles/Profile.css";
 import logo from "../assets/logo.png";
 import buttonPartners from "../assets/buttonPartners.png";
