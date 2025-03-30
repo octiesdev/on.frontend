c82950d0dd1e7d3390e0d432de3c00ab53223932
НовыйИмя <Email>
2025-03-30 20:03:09 +0200

    freeONEX

--- src/pages/PartnerButton.jsx
+++ src/pages/PartnerButton.jsx
@@ -14,7 +14,7 @@ const PartnerButton = () => {
     fetch(`https://1xback-production.up.railway.app/get-ambassador-data?userId=${userId}`)
       .then(res => res.json())
       .then(data => {
-        setHasAccess(data.hasAmbassadorAccess);
+        setHasAccess(data.hasAccess);
       })
       .catch(err => {
         console.error("❌ Ошибка при проверке ambassador-доступа:", err);
