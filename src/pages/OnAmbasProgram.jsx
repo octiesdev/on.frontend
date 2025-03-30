98cb5af2b41fc2f217874e96c3360ea344d377a8
НовыйИмя <Email>
2025-03-30 19:04:39 +0200

    freeONEX

--- src/pages/OnAmbasProgram.jsx
+++ src/pages/OnAmbasProgram.jsx
@@ -129,7 +129,7 @@ const OnAmbasProgram = () => {
                   <div className={`friend-eclipse${index === 1 ? "-v2" : index === 2 ? "-v3" : ""}`}> 
                     <img src={logoInTheEclipseIMG} alt=""/>
                   </div>
-                  <h2>{ref.username}</h2>
+                  <h2>{ref.username.replace(/^@/, "")}</h2>
                 </div>
                 <div className="OnAmbasProgram-section-friendsBlocks"> 
                   <div className="rewardInTON-block">
