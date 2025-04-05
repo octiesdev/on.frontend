b5675eabc072568f6655c290f14cca279ad2196d
НовыйИмя <Email>
2025-04-05 20:41:41 +0200

    mainnet

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -20,6 +20,9 @@ import onexlogoIMG from "../assets/onex-img-all.png";
 const API_URL = "https://1xback-production.up.railway.app"; 
 
 const Profile = () => {
+  if (!userId) {
+    return null; // или можно заменить на <LoadingScreen />
+  }
     const { userId, balance, onexBalance, fetchBalance } = useUser(); 
     const [activeSection, setActiveSection] = useState("default");
 
